848a54b784bd64dd0ccbf2a31b39d407d69e8016
Nicolas Moreau <nicolas.moreau@asmozia.com>
2024-07-29 16:12:34 +0200

    feat: export jsonrpc-client in a new package

--- examples/react-hook-example/src/app/app.tsx
+++ examples/react-hook-example/src/app/app.tsx
@@ -2,17 +2,17 @@ import { OdooClientProvider, OdooClient } from '@asmozia/react-odoo';
 
 import { ReactHookSimplePage } from './ReactHookSimplePage';
 
-const odooClient = new OdooClient({
-  dbName: 'db-demo', // This is not a real db dame for hostname demo4.odoo.com, to get a real db name, you need to create a new database on demo4.odoo.com
-  hostname: 'localhost',
-  port: '4200',
-  pathname: 'jsonrpc',
-  unsecure: true,
-});
-
 export function App() {
   return (
-    <OdooClientProvider client={odooClient}>
+    <OdooClientProvider
+      defaultConfig={{
+        dbName: 'db-demo', // This is not a real db dame for hostname demo4.odoo.com, to get a real db name, you need to create a new database on demo4.odoo.com
+        hostname: 'localhost',
+        port: '4200',
+        pathname: 'jsonrpc',
+        unsecure: true,
+      }}
+    >
       <ReactHookSimplePage />
     </OdooClientProvider>
   );
